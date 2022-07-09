1c5ce0140bee8366b0df3e1c028de69329171dd1
Marco <kutscha.marco@gmail.com>
2022-07-09 19:08:11 +0200

    Added examples

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import usb.core
 import usb.util
+import os
 
 
-VENDOR_ID = 0x0461 #Example ID
-PRODUCT_ID = 0x0010 #Example ID
+VENDOR_ID = 0x413C #Example ID
+PRODUCT_ID = 0x2105 #Example ID
 
 device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
 device.set_configuration()
@@ -20,4 +21,16 @@ while True:
             if e.args == ('Operation timed out',):
                 attempts -= 1
                 continue
-    print(data) 
+
+    i = 2 #Might be different for your device
+    if data[i] != 0: 
+        print("Data:", data[i])
+
+        if data[i] == 4:
+            os.system("start chrome")
+        elif data[i] == 5:
+            os.system("start firefox")
+        elif data[i] == 6:
+            os.system("start https://github.com")
+        #Add your functions here
+
